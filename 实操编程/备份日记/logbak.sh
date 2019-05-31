916995a1c007d07d6f09142482fdcfba3b7ccf41
zhaiyanquan <email>
2019-05-31 16:20:21 +0800

    delete 实操编程

--- 实操编程/备份日记/logbak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#功能：使用 tar 命令备份/var/log 下的所有日志文件
-#编写备份脚本,备份后的文件名包含日期标签,防止后面的备份将前面的备份数据覆盖 
-
-tar -czf log-`date +%Y%m%d`.tar.gz /var/log
-
-#或tar -czf log-$(date +%Y%m%d).tar.gz /var/log
-
-
